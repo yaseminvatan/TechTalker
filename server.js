5543ed8ea80b2064bd715463783a3f2e1e0f9771
yaseminvatan <66827861+yaseminvatan@users.noreply.github.com>
2024-12-23 12:23:56 -0800

    feat: integrate session handling, Handlebars templating, and Sequelize session store setup.

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const path = require('path');
