f1ab2c2acf6173c37d2aa155c30b71e2c87b18d5
yaseminvatan <66827861+yaseminvatan@users.noreply.github.com>
2024-12-23 20:36:30 -0800

    feat: implement logout functionality
    - Added event listener to handle logout button click
    - Sent POST request to the logout API to end the session
    - Redirected to the homepage on successful logout
    - Displayed an alert for failed logout attempts

---
+++ public/js/logout.js
@@ -0,0 +1,13 @@
+document.querySelector('#logout').addEventListener('click', async () => {
+    const response = await fetch('/api/users/logout', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to log out.');
+    }
+  });
+  
