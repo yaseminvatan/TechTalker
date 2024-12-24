7258c3074314c221c78e6d560d0fb45496e0ec33
yaseminvatan <66827861+yaseminvatan@users.noreply.github.com>
2024-12-23 21:45:54 -0800

    fix: unnecessary code piece deleted

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -2,48 +2,6 @@ const router = require('express').Router();
 const { Post, User, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-// Get all posts
-router.get('/', async (req, res) => {
-  try {
-    const postData = await .findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['username'],
-        },
-      ],
-    });
-    res.status(200).json(postData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// Get a single post
-router.get('/:id', async (req, res) => {
-  try {
-    const postData = await Post.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ['username'],
-        },
-        {
-          model: Comment,
-          include: [{ model: User, attributes: ['username'] }],
-        },
-      ],
-    });
-    if (!postData) {
-      res.status(404).json({ message: 'No post found with this id!' });
-      return;
-    }
-    res.status(200).json(postData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
 // Create a new post
 router.post('/', withAuth, async (req, res) => {
     try {
